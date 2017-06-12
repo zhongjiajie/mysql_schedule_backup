9c55c7e1cc5eb330af7512c4afc935331721f85c
zhongjiajie <zhongjiajie955@hotmail.com>
2017-06-12 18:02:29 +0800

    mysql_schedule_backup配置文件（除了备份时间）

---
+++ conf.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+job = {
+    "setting": {
+        # 备份的根目录
+        "root": "F:/mysql_schedule_backup",
+        # 备份保持的时间
+        "keep_day": 60,
+        # 是否压缩备份
+        "is_zip": "true"
+    },
+    "db_connection": [
+        # 每一个dict对应一个IP:PORT
+        {
+            "usr": "root",
+            "pwd": "mysql",
+            "host": "127.0.0.1",
+            "port": "3306",
+            "db_and_table": {
+                # "backup_database": ["backup_table_1", "backup_table_2", ...]
+                "tmp": [
+                    "tb1",
+                    "tb2"
+                ],
+                # "backup_database": ["*"]   # *默认全库备份
+                "world": [
+                    "*"
+                ]
+            }
+        },
+        {
+            "usr": "root",
+            "pwd": "mysql",
+            "host": "127.0.0.1",
+            "port": "3306",
+            "db_and_table": {
+                # "backup_database": ["backup_table_1", "backup_table_2", ...]
+                "sakila": [
+                    "*"
+                ]
+            }
+        }
+    ]
+}
